a456eb1e40fdb676a2c5b0c5c8395aaa9ec6c0d4
chungvu <chung.vu@chudu24.com>
2024-05-03 16:48:52 +0700

    cdoe

--- src/modules/profile/components/card_project.tsx
+++ src/modules/profile/components/card_project.tsx
@@ -1,12 +1,24 @@
 import { Component, ReactNode } from "react";
 import styles from '../index.module.css';
 
-export default class CartProject extends Component {
+type CartProjectProps = {
+  title: string;
+  thumbnail: string;
+  images: Array<string>;
+  datetime: string;
+  description: string;
+}
 
+export default class CartProject extends Component<CartProjectProps> {
   render(): ReactNode {
     return (
       <div className={styles.item_project}>
-        <p>cart</p>
+        <img src={this.props.thumbnail} alt={this.props.title} className={styles.img_thumbnail} />
+        <div className={styles.box_project_info}>
+          <p><i>Name:</i> {this.props.title}</p>
+          <p><i>Duration:</i> {this.props.datetime}</p>
+          <p><i>Description:</i> {this.props.description}</p>
+        </div>
       </div>
     )
   }
--- src/modules/profile/index.tsx
+++ src/modules/profile/index.tsx
@@ -6,9 +6,27 @@ export default class Profile extends Component {
   render(): ReactNode {
     return (
       <div className={styles.box}>
-        <CartProject />
-        <CartProject />
-        <CartProject />
+        <CartProject
+          title={'01'}
+          images={[]}
+          thumbnail={'https://placehold.co/600x600'}
+          description={''}
+          datetime={'11/2023 - 03/2024'}
+        />
+        <CartProject
+          title={'01'}
+          images={[]}
+          thumbnail={'https://placehold.co/600x600'}
+          description={''}
+          datetime={'11/2023 - 03/2024'}
+        />
+        <CartProject
+          title={'01'}
+          images={[]}
+          thumbnail={'https://placehold.co/600x600'}
+          description={''}
+          datetime={'11/2023 - 03/2024'}
+        />
       </div>
     )
   }
